ba8456f4fc77815c9437482ec0483e9a77647696
Rudresh L Thakur <rudresh04thakur@gmail.com>
2021-05-20 14:20:43 +0530

    Bootstrap added

---
+++ bootstrap/js/script.js
@@ -0,0 +1,43 @@
+// javascript is programming lang
+// jquery is javascript lab
+
+// var -- it is global for program
+// let -- it block level
+// const -- it block level , you can not reasing the value
+
+// window.onload = function(){
+   
+// }
+
+
+// function <funName>([param]){
+//     function defination
+// }
+
+// function add(a,b){
+//     console.log("Addition is - "+(a+b));
+// }
+// add(10,45);
+
+// anonymous function
+
+// var <varname> = function([params]) {
+//     function defination
+// }
+
+// var sub = function(a,b) {
+//     console.log("Substraction -- ",(a-b))
+// }
+// sub(10,5)
+
+// arrow or lambda function 
+
+// var <varname> = ([params])=>{
+//     function defination
+// }
+
+
+// var div = (a,b)=>{
+//     console.log("Division -- ",(a/b));
+// }
+// div(10,2)
